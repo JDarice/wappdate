db6957dbe64bc377ed7ede9189cafb0664ba9dbc
guerradanilo <53982976+guerradanilo@users.noreply.github.com>
2020-09-08 00:43:08 -0300

    ajustes na dashboard e página de contatos; ajuste de responsividade para tablet

---
+++ public/javascripts/contatoDOM.js
@@ -0,0 +1,10 @@
+
+function criarContato() {
+    document.getElementById("main-block-component-body-detail").style.visibility = "visible";
+    document.getElementById("main-block-component-body-detail").style.display = "block";
+  }
+ 
+  function cancelarCriarContato() {
+    document.getElementById("main-block-component-body-detail").style.visibility = "hidden";
+    document.getElementById("main-block-component-body-detail").style.display = "hidden";
+  }
