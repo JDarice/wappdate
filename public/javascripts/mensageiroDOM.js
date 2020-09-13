fc644334018dc45e0251648f9c00061573e334ab
guerradanilo <53982976+guerradanilo@users.noreply.github.com>
2020-09-13 19:07:20 -0300

    ajustes no css + página de configuração do mensageiro ainda em desenvolvimento

---
+++ public/javascripts/mensageiroDOM.js
@@ -0,0 +1,81 @@
+
+function mensageiroIniciar() {
+    document.getElementById("main-block-component-body-card-form-0").classList.add('main-block-component-body-card-form-hidden');
+    document.getElementById("main-block-component-body-card-form-0").classList.remove('main-block-component-body-card-form');
+    document.getElementById("main-block-component-body-card-left-column-1").classList.add('main-block-component-body-card-left-column-hidden');
+    document.getElementById("main-block-component-body-card-left-column-1").classList.remove('main-block-component-body-card-left-column');
+    document.getElementById("main-block-component-body-card-left-column-index-1").classList.add('main-block-component-body-card-left-column-index-hidden');
+    document.getElementById("main-block-component-body-card-left-column-index-1").classList.remove('main-block-component-body-card-left-column-index');
+    document.getElementById("main-block-component-body-card-left-column-button-1").classList.add('main-block-component-body-card-left-column-button-hidden');
+    document.getElementById("main-block-component-body-card-left-column-button-1").classList.remove('main-block-component-body-card-left-column-button');
+    document.getElementById("main-block-component-body-card-form-1").classList.add('main-block-component-body-card-form');
+    document.getElementById("main-block-component-body-card-form-1").classList.remove('main-block-component-body-card-form-hidden');
+    document.getElementById("main-block-component-body-card-left-column-button-2").classList.add('main-block-component-body-card-left-column-button');    
+    document.getElementById("main-block-component-body-card-left-column-button-2").classList.remove('main-block-component-body-card-left-column-button-hidden');    
+}
+
+function mensageiro2() {
+    document.getElementById("main-block-component-body-card-form-1").classList.add('main-block-component-body-card-form-hidden');
+    document.getElementById("main-block-component-body-card-form-1").classList.remove('main-block-component-body-card-form');
+    document.getElementById("main-block-component-body-card-left-column-2").classList.add('main-block-component-body-card-left-column-hidden');
+    document.getElementById("main-block-component-body-card-left-column-2").classList.remove('main-block-component-body-card-left-column');
+    document.getElementById("main-block-component-body-card-left-column-index-2").classList.add('main-block-component-body-card-left-column-index-hidden');
+    document.getElementById("main-block-component-body-card-left-column-index-2").classList.remove('main-block-component-body-card-left-column-index');
+    document.getElementById("main-block-component-body-card-left-column-button-2").classList.add('main-block-component-body-card-left-column-button-hidden');
+    document.getElementById("main-block-component-body-card-left-column-button-2").classList.remove('main-block-component-body-card-left-column-button');
+    document.getElementById("main-block-component-body-card-form-2").classList.add('main-block-component-body-card-form');
+    document.getElementById("main-block-component-body-card-form-2").classList.remove('main-block-component-body-card-form-hidden');
+    document.getElementById("main-block-component-body-card-left-column-button-3").classList.add('main-block-component-body-card-left-column-button');    
+    document.getElementById("main-block-component-body-card-left-column-button-3").classList.remove('main-block-component-body-card-left-column-button-hidden');    
+}
+
+function mensageiro3() {
+    document.getElementById("main-block-component-body-card-form-2").classList.add('main-block-component-body-card-form-hidden');
+    document.getElementById("main-block-component-body-card-form-2").classList.remove('main-block-component-body-card-form');
+    document.getElementById("main-block-component-body-card-left-column-3").classList.add('main-block-component-body-card-left-column-hidden');
+    document.getElementById("main-block-component-body-card-left-column-3").classList.remove('main-block-component-body-card-left-column');
+    document.getElementById("main-block-component-body-card-left-column-index-3").classList.add('main-block-component-body-card-left-column-index-hidden');
+    document.getElementById("main-block-component-body-card-left-column-index-3").classList.remove('main-block-component-body-card-left-column-index');
+    document.getElementById("main-block-component-body-card-left-column-button-3").classList.add('main-block-component-body-card-left-column-button-hidden');
+    document.getElementById("main-block-component-body-card-left-column-button-3").classList.remove('main-block-component-body-card-left-column-button');
+    document.getElementById("main-block-component-body-card-form-3").classList.add('main-block-component-body-card-form');
+    document.getElementById("main-block-component-body-card-form-3").classList.remove('main-block-component-body-card-form-hidden');
+    document.getElementById("main-block-component-body-card-left-column-button-4").classList.add('main-block-component-body-card-left-column-button');    
+    document.getElementById("main-block-component-body-card-left-column-button-4").classList.remove('main-block-component-body-card-left-column-button-hidden');    
+}
+
+function mensageiro4() {
+    document.getElementById("main-block-component-body-card-form-3").classList.add('main-block-component-body-card-form-hidden');
+    document.getElementById("main-block-component-body-card-form-3").classList.remove('main-block-component-body-card-form');
+    document.getElementById("main-block-component-body-card-left-column-4").classList.add('main-block-component-body-card-left-column-hidden');
+    document.getElementById("main-block-component-body-card-left-column-4").classList.remove('main-block-component-body-card-left-column');
+    document.getElementById("main-block-component-body-card-left-column-index-4").classList.add('main-block-component-body-card-left-column-index-hidden');
+    document.getElementById("main-block-component-body-card-left-column-index-4").classList.remove('main-block-component-body-card-left-column-index');
+    document.getElementById("main-block-component-body-card-left-column-button-4").classList.add('main-block-component-body-card-left-column-button-hidden');
+    document.getElementById("main-block-component-body-card-left-column-button-4").classList.remove('main-block-component-body-card-left-column-button');
+    document.getElementById("main-block-component-body-card-form-4").classList.add('main-block-component-body-card-form');
+    document.getElementById("main-block-component-body-card-form-4").classList.remove('main-block-component-body-card-form-hidden');
+    document.getElementById("main-block-component-body-card-left-column-button-5").classList.add('main-block-component-body-card-left-column-button');    
+    document.getElementById("main-block-component-body-card-left-column-button-5").classList.remove('main-block-component-body-card-left-column-button-hidden');    
+}
+
+function mensageiro5() {
+    document.getElementById("main-block-component-body-card-form-4").classList.add('main-block-component-body-card-form-hidden');
+    document.getElementById("main-block-component-body-card-form-4").classList.remove('main-block-component-body-card-form');
+    document.getElementById("main-block-component-body-card-left-column-5").classList.add('main-block-component-body-card-left-column-hidden');
+    document.getElementById("main-block-component-body-card-left-column-5").classList.remove('main-block-component-body-card-left-column');
+    document.getElementById("main-block-component-body-card-left-column-5").classList.remove('main-block-component-body-card-border-radius-right');
+    document.getElementById("main-block-component-body-card-left-column-index-5").classList.add('main-block-component-body-card-left-column-index-hidden');
+    document.getElementById("main-block-component-body-card-left-column-index-5").classList.remove('main-block-component-body-card-left-column-index');
+    document.getElementById("main-block-component-body-card-left-column-button-5").classList.add('main-block-component-body-card-left-column-button-hidden');
+    document.getElementById("main-block-component-body-card-left-column-button-5").classList.remove('main-block-component-body-card-left-column-button');
+    document.getElementById("main-block-component-body-card-form-5").classList.add('main-block-component-body-card-form');
+    document.getElementById("main-block-component-body-card-form-5").classList.add('main-block-component-body-card-border-radius-right');
+    document.getElementById("main-block-component-body-card-form-5").classList.remove('main-block-component-body-card-form-hidden');
+
+}
+
+function cancelarCriarContato() {
+    document.getElementById("main-block-component-body-detail").style.visibility = "hidden";
+    document.getElementById("main-block-component-body-detail").style.display = "hidden";
+}
