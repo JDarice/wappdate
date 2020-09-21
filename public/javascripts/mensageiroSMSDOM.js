d209f05cb178af5ecd7624807a601e0f4de79db8
guerradanilo <53982976+guerradanilo@users.noreply.github.com>
2020-09-21 01:12:25 -0300

    tela configuração mensageiro SMS finalizada

--- public/javascripts/mensageiroSMSDOM.js
+++ public/javascripts/mensageiroSMSDOM.js
@@ -10,6 +10,9 @@ function mensageiroIniciar() {
     document.getElementById("main-block-component-body-card-left-column-button-1").classList.remove('main-block-component-body-card-left-column-button');
     document.getElementById("main-block-component-body-card-form-1").classList.add('main-block-component-body-card-form');
     document.getElementById("main-block-component-body-card-form-1").classList.remove('main-block-component-body-card-form-hidden');
+}
+
+function mostrarbotaomensageiro2() {
     document.getElementById("main-block-component-body-card-left-column-button-2").classList.add('main-block-component-body-card-left-column-button');    
     document.getElementById("main-block-component-body-card-left-column-button-2").classList.remove('main-block-component-body-card-left-column-button-hidden');    
 }
@@ -25,6 +28,9 @@ function mensageiro2() {
     document.getElementById("main-block-component-body-card-left-column-button-2").classList.remove('main-block-component-body-card-left-column-button');
     document.getElementById("main-block-component-body-card-form-2").classList.add('main-block-component-body-card-form');
     document.getElementById("main-block-component-body-card-form-2").classList.remove('main-block-component-body-card-form-hidden');
+}
+
+function mostrarbotaomensageiro3() {
     document.getElementById("main-block-component-body-card-left-column-button-3").classList.add('main-block-component-body-card-left-column-button');    
     document.getElementById("main-block-component-body-card-left-column-button-3").classList.remove('main-block-component-body-card-left-column-button-hidden');    
 }
@@ -33,58 +39,45 @@ function mensageiro3() {
     document.getElementById("main-block-component-body-card-form-2").classList.add('main-block-component-body-card-form-hidden');
     document.getElementById("main-block-component-body-card-form-2").classList.remove('main-block-component-body-card-form');
     document.getElementById("main-block-component-body-card-left-column-3").classList.add('main-block-component-body-card-left-column-hidden');
+    document.getElementById("main-block-component-body-card-left-column-3").classList.remove('main-block-component-body-card-border-radius-right');
     document.getElementById("main-block-component-body-card-left-column-3").classList.remove('main-block-component-body-card-left-column');
     document.getElementById("main-block-component-body-card-left-column-index-3").classList.add('main-block-component-body-card-left-column-index-hidden');
     document.getElementById("main-block-component-body-card-left-column-index-3").classList.remove('main-block-component-body-card-left-column-index');
     document.getElementById("main-block-component-body-card-left-column-button-3").classList.add('main-block-component-body-card-left-column-button-hidden');
     document.getElementById("main-block-component-body-card-left-column-button-3").classList.remove('main-block-component-body-card-left-column-button');
+    document.getElementById("main-block-component-body-card-form-3").classList.add('main-block-component-body-card-border-radius-right');
     document.getElementById("main-block-component-body-card-form-3").classList.add('main-block-component-body-card-form');
     document.getElementById("main-block-component-body-card-form-3").classList.remove('main-block-component-body-card-form-hidden');
-    document.getElementById("main-block-component-body-card-left-column-button-4").classList.add('main-block-component-body-card-left-column-button');    
-    document.getElementById("main-block-component-body-card-left-column-button-4").classList.remove('main-block-component-body-card-left-column-button-hidden');    
-}
-
-function mensageiro4() {
-    document.getElementById("main-block-component-body-card-form-3").classList.add('main-block-component-body-card-form-hidden');
-    document.getElementById("main-block-component-body-card-form-3").classList.remove('main-block-component-body-card-form');
-    document.getElementById("main-block-component-body-card-left-column-4").classList.add('main-block-component-body-card-left-column-hidden');
-    document.getElementById("main-block-component-body-card-left-column-4").classList.remove('main-block-component-body-card-left-column');
-    document.getElementById("main-block-component-body-card-left-column-index-4").classList.add('main-block-component-body-card-left-column-index-hidden');
-    document.getElementById("main-block-component-body-card-left-column-index-4").classList.remove('main-block-component-body-card-left-column-index');
-    document.getElementById("main-block-component-body-card-left-column-button-4").classList.add('main-block-component-body-card-left-column-button-hidden');
-    document.getElementById("main-block-component-body-card-left-column-button-4").classList.remove('main-block-component-body-card-left-column-button');
-    document.getElementById("main-block-component-body-card-form-4").classList.add('main-block-component-body-card-form');
-    document.getElementById("main-block-component-body-card-form-4").classList.remove('main-block-component-body-card-form-hidden');
-    document.getElementById("main-block-component-body-card-left-column-button-5").classList.add('main-block-component-body-card-left-column-button');    
-    document.getElementById("main-block-component-body-card-left-column-button-5").classList.remove('main-block-component-body-card-left-column-button-hidden');    
-}
-
-function mensageiro5() {
-    document.getElementById("main-block-component-body-card-form-4").classList.add('main-block-component-body-card-form-hidden');
-    document.getElementById("main-block-component-body-card-form-4").classList.remove('main-block-component-body-card-form');
-    document.getElementById("main-block-component-body-card-left-column-5").classList.add('main-block-component-body-card-left-column-hidden');
-    document.getElementById("main-block-component-body-card-left-column-5").classList.remove('main-block-component-body-card-left-column');
-    document.getElementById("main-block-component-body-card-left-column-5").classList.remove('main-block-component-body-card-border-radius-right');
-    document.getElementById("main-block-component-body-card-left-column-index-5").classList.add('main-block-component-body-card-left-column-index-hidden');
-    document.getElementById("main-block-component-body-card-left-column-index-5").classList.remove('main-block-component-body-card-left-column-index');
-    document.getElementById("main-block-component-body-card-left-column-button-5").classList.add('main-block-component-body-card-left-column-button-hidden');
-    document.getElementById("main-block-component-body-card-left-column-button-5").classList.remove('main-block-component-body-card-left-column-button');
-    document.getElementById("main-block-component-body-card-form-5").classList.add('main-block-component-body-card-form');
-    document.getElementById("main-block-component-body-card-form-5").classList.add('main-block-component-body-card-border-radius-right');
-    document.getElementById("main-block-component-body-card-form-5").classList.remove('main-block-component-body-card-form-hidden');
-
-}
-
-function cancelarCriarContato() {
-    document.getElementById("main-block-component-body-detail").style.visibility = "hidden";
-    document.getElementById("main-block-component-body-detail").style.display = "hidden";
 }
 
 function countChar(val) {
-    var totalSMSChar = 160;
-    var len = val.value.length;
-    var availableChar = totalSMSChar - len;
-    var smsLength1 = "Ao usar este nome, ainda vão restar ";
-    var smsLength2 = " caracteres para sua mensagem de SMS.";
+    let businessFromName = val.value.toUpperCase();
+    let len = val.value.length;
+    let totalSMSChar = 160;
+    let availableChar = totalSMSChar - len;
+    let smsLength1 = "Ao usar este nome, ainda vão restar <strong>";
+    let smsLength2 = " caracteres</strong> para sua mensagem de SMS.";
     document.getElementById("charNum").innerHTML = smsLength1 + availableChar + smsLength2;
-};
+}
+
+function orderStartedCountChar(val){
+    let businessFromName = "[" + document.getElementById("businessFromName").value.toUpperCase() + "]: ";
+    let businessFromNameLen = businessFromName.length;
+    let messageOrderStarted = val.value;
+    let totalSMSChar = 160;
+    let len = val.value.length;
+    let availableChar = "Catacteres disponíveis: ";
+    let availableCharCount = totalSMSChar - businessFromNameLen - len;
+    document.getElementById("orderStarted").innerHTML = "Sua mensagem: " + "<strong>" + businessFromName + messageOrderStarted + "</strong><br>" + availableChar + "<strong>" + availableCharCount + "</strong>";
+}
+
+function orderReadyCountChar(val){
+    let businessFromName = "[" + document.getElementById("businessFromName").value.toUpperCase() + "]: ";
+    let businessFromNameLen = businessFromName.length;
+    let messageOrderStarted = val.value;
+    let totalSMSChar = 160;
+    let len = val.value.length;
+    let availableChar = "Catacteres disponíveis: ";
+    let availableCharCount = totalSMSChar - businessFromNameLen - len;
+    document.getElementById("orderReady").innerHTML = "Sua mensagem: " + "<strong>" + businessFromName + messageOrderStarted + "</strong><br>" + availableChar + "<strong>" + availableCharCount + "</strong>";
+}
